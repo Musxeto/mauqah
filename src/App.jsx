236747fbb87a7ce59ed75b419abcf83d89a690f6
musxeto <mustafamalikawan786@gmail.com>
2024-12-24 20:55:32 +0500

    more setup and boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
+import AllRoutes from './routes/AllRoutes'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div><AllRoutes /></div>
+    
   )
 }
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AboutUs from '../pages/AboutUs'
-import Homepage from '../pages/Homepage'
-import Contact from '../pages/Contact'
-import WazirabadFieldStudy from '../pages/WazirabadFieldStudy'
-import VisitToJajja from '../pages/VisitToJajja'
-import GenderEmpowermentSociety from '../pages/GenderEmpowermentSociety'
-import Resources from '../pages/Resources'
+import Homepage from '../pages/Homepage';
+const AboutUs = React.lazy(() => import('../pages/AboutUs'));
+const Contact = React.lazy(() => import('../pages/Contact'));
+const WazirabadFieldStudy = React.lazy(() => import('../pages/WazirabadFieldStudy'));
+const VisitToJajja = React.lazy(() => import('../pages/VisitToJajja'));
+const GenderEmpowermentSociety = React.lazy(() => import('../pages/GenderEmpowermentSociety'));
+const Resources = React.lazy(() => import('../pages/Resources'));
+const NotFound = React.lazy(() => import('../pages/NotFound'));
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Loading from 'react-loading';
 const AllRoutes = () => {
   return(
     <BrowserRouter>
+    <Navbar />
+    <Suspense
+        fallback={
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+              backgroundColor: '#f8f9fa',
+            }}
+          >
+            <Loading type="spin" color="#A566F7" height={50} width={50} />
+          </div>
+        }
+      ></Suspense>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/about' element={<AboutUs />} />
@@ -20,6 +40,7 @@ const AllRoutes = () => {
         <Route path='resources' element={<Resources />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
