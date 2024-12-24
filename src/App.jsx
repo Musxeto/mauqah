ed03f54d40fd6aad0523c82d58d65303434b1bc0
musxeto <mustafamalikawan786@gmail.com>
2024-12-24 20:56:32 +0500

    more setup and boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
-import AllRoutes from './routes/AllRoutes'
+import "./App.css";
+import AllRoutes from "./routes/AllRoutes";
 
 export default function App() {
   return (
-    <div><AllRoutes /></div>
-    
-  )
+    <div>
+      <AllRoutes />
+    </div>
+  );
 }
