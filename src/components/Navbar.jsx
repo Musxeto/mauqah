3ddb8c467c3e23c7393920fd81bfa00a1f2a51ca
musxeto <mustafamalikawan786@gmail.com>
2024-12-28 01:26:28 +0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -147,14 +147,14 @@ const Navbar = () => {
           <Link
             to="/about"
             onClick={() => setIsOpen(false)}
-            className="block font-semibold hover:bg-[#FF6AA4] px-4 py-2 rounded"
+            className="block font-semibold hover:bg-yellow-200 px-4 py-2 rounded"
           >
             About
           </Link>
           <Link
             to="/resources"
             onClick={() => setIsOpen(false)}
-            className="block font-semibold hover:bg-[#FF6AA4] px-4 py-2 rounded"
+            className="block font-semibold hover:bg-yellow-200 px-4 py-2 rounded"
           >
             Resources
           </Link>
