cd8e1bab000e064949b33ffaeb91f66201aef12d
musxeto <mustafamalikawan786@gmail.com>
2024-12-29 22:24:48 +0500

    smooth transiton

--- src/routes/ScrollToTop.jsx
+++ src/routes/ScrollToTop.jsx
@@ -5,8 +5,12 @@ const ScrollToTop = () => {
   const location = useLocation();
 
   useEffect(() => {
-    
-    if (!location.hash) {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    } else {
       window.scrollTo(0, 0);
     }
   }, [location]);
